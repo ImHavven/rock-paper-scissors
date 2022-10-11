68391a47eccd6fb8f11fa15530ec8c743a3860f0
ImHavven <havven1@hotmail.com>
2022-10-11 18:26:39 +0500

    some interesting changes

--- r-p-s.js
+++ r-p-s.js
@@ -1,39 +1,83 @@
+const btns = document.querySelectorAll('.btn');
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay() {
     var ran = ['Rock','Paper','Scissors'],
         ran1 = ran[Math.floor(Math.random() * ran.length)];
     return ran1;
 };
 
-function gameChoices(playerSelection,computerSelection) {
+function disableBtn(){
+    btns.forEach(btn => {
+        btn.disabled = true;
+    })
+}
+
+
+function gameChoices(playerSelection, computerSelection) {
+    let result = '';
     if (playerSelection == 'Rock' && computerSelection == 'Scissors' ||
     playerSelection == 'Scissors' && computerSelection === 'Paper'||
     playerSelection == 'Paper' && computerSelection == 'Rock') {
-        return 'You win Player';
-    } else if (playerSelection == 'Scissors' && computerSelection == 'Rock'||
-    playerSelection == 'Paper' && computerSelection == 'Scissors'||
-    playerSelection == 'Rock' && computerSelection == 'Paper'){
-        return 'Computer Wins';
+        playerScore++;
+        result = ('You beaten ' + computerSelection + ' by choosing ' + playerSelection
+                    + '<br>player Score: ' + playerScore + '<br>computer Score: ' + computerScore);
+        if (playerScore == 5){
+            result = '<br><br>You Win The Game';
+            disableBtn();
+        }
+    } else if (playerSelection == computerSelection){
+        computerScore++;
+        playerScore++;
+        result = ('you both choose ' + playerSelection
+                + '<br>player Score: ' + playerScore + '<br>computer Score: ' + computerScore);
+            if(playerScore == 5 && computerScore == 5) {
+                result = '<br><br>It\'s Draw';
+                disableBtn();
+            }
     } else {
-        return 'Draw';
+        computerScore++;
+        result = ('You have been beaten by ' + computerSelection + ' by choosing ' + playerSelection
+                    + '<br>player Score: ' + playerScore + '<br>computer Score: ' + computerScore);
+        if(computerScore == 5){
+            result = '<br><br>The Computer Win the game ';
+            disableBtn();
+        }
     };
+    const resultRound = document.getElementById('result').innerHTML = result;
+    return resultRound;
 };
 
-let playerScore = 0;
-let computerScore = 0;
 
-function gamePlay(){
-    for (let i = 0; i < 5; i++){
-        let plays =  prompt("What's your play? (rock, paper, scissor)", "");
-        let plays1 = plays.toLowerCase();
-        let playerSelection = plays1;
-        let computerSelection = computerPlay();
-        console.log(gameChoices(playerSelection, computerSelection));
-    };
-    if (playerScore == computerScore) {
-        return "It's a Draw";
-    } else if (computerScore > playerScore) {
-        return "Computer Wins";
-    } else {
-        return "You Win the game player";
-    };
-};
+btns.forEach(button => {
+    button.addEventListener('click', () =>{
+        let playerChoice = button.getAttribute('id');
+        gameChoices(playerChoice, computerPlay());
+    });
+});
+
+
+
+
+//This was for the game to be played on the console 
+
+
+//function gamePlay(){
+//    if (playerScore == computerScore) {
+//        return "It's a Draw";
+//    } else if (computerScore > playerScore) {
+//        return "Computer Wins";
+//    } else {
+//        return "You Win the game player";
+//    };
+//};-
+
+
+//    for (let i = 0; i < 5; i++){
+//        let plays =  prompt("What's your play? (rock, paper, scissor)", "");
+//        let plays1 = plays.toLowerCase();
+//        let playerSelection = plays1;
+//        let computerSelection = computerPlay();
+//        console.log(gameChoices(playerSelection, computerSelection));
+//    };
