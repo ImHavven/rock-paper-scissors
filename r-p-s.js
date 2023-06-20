588d09ee4e25810837831b3d707e9dbae5ecbf5e
Hassan(HAVVEN) <103832741+ImHavven@users.noreply.github.com>
2023-06-20 23:00:07 +0500

    Update some Comments

--- r-p-s.js
+++ r-p-s.js
@@ -65,7 +65,7 @@ btns.forEach((btn) => {
 
 
 
-//This Func Was Used Before To play The Game In The Console
+//This Func Was Used to play the game in the Console...
 
 //function game(){
 //    if(playerScore == computerScore){
